9ed7fa398d81367b8cceb699affac81967e38175
Alex <147089224+PsyArd@users.noreply.github.com>
2024-03-29 17:40:31 +0300

    Add files via upload

---
+++ lesson7dz/src/main/java/ru/geekbrains/oop/lesson7/builder/Program.java
@@ -0,0 +1,26 @@
+package ru.geekbrains.oop.lesson7.builder;
+
+public class Program {
+
+    public static void main(String[] args) {
+
+        StringBuffer stringBuffer = new StringBuffer();
+        @SuppressWarnings("unused")
+        String s  = stringBuffer
+                .append("AAA")
+                .append(1)
+                .append("BBB")
+                .insert(2, "DDD")
+                .toString();
+
+        @SuppressWarnings("unused")
+        Order order1 = new OrderBuilder()
+                .setClientName("Client #1")
+                .setPrice(200.5)
+                .setQnt(4)
+                .setProductId(12454)
+                .setSign(true)
+                .build();
+    }
+
+}
---
+++ lesson7dz/src/main/java/ru/geekbrains/oop/lesson7/factory/Test.java
@@ -0,0 +1,29 @@
+package ru.geekbrains.oop.lesson7.factory;
+
+public class Test extends Employee {
+    private Test(String name, String surName, double salary) {
+        super(name, surName, salary);
+    }
+
+    public static Test create(String name, String surName, double salary){
+
+        if (name == null || name.length() < 3)
+            throw new RuntimeException("Имя тестировщика указано некорректно.");
+ 
+        if (salary <= 0)
+            throw new RuntimeException("Уровень заработной платы указан некорректно.");
+
+        return new Test(name, surName, salary);
+    }
+
+    @Override
+    public double calculateSalary() {
+        return salary;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s %s; Тестировщик; Среднемесячная заработная плата (фиксированная месячная оплата): %.2f (руб.)",
+                surName, name, salary);
+    }
+}
